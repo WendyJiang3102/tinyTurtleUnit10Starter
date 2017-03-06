a339db38420d79b8f8f713f13a80ceda0e6c5124
Wendy Jiang <wjiang.student@hsfi.us>
2017-03-06 22:08:54 +0000

    added parameters

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
-function square(){
-forward(50);
+function house(size){
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
+right(45);
+forward(70);
+right(90);
+forward(70);
+right(135);
+forward(size);
+
+}
+function hexagon(x){
+forward(x);
+right(60);
+forward(x);
+right(60);
+forward(x);
+right(60);
+forward(x);
+right(60);
+forward(x);
+right(60);
+forward(x);
+right(60);
 }
 
 // Type your function call below
-
+house(100);
+hexagon(50);
 
 stamp();

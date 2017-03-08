818c1e39468423e9483987a44df964d4407fda2e
Wendy Jiang <wjiang.student@hsfi.us>
2017-03-08 22:06:11 +0000

    new

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
-function house(size){
-forward(size);
-right(90);
-forward(size);
-right(90);
-forward(size);
-right(90);
-forward(size);
-right(90);
-forward(size);
-right(45);
-forward(70);
-right(90);
-forward(70);
-right(135);
-forward(size);
 
+function square(size){
+forward(size);
+right(90);
+forward(size);
+right(90);
+forward(size);
+right(90);
+forward(size);
+right(90);
+forward(size);
 }
+
+function triangle(y){
+right(45);
+forward(y);
+right(90);
+forward(y);
+right(135);
+forward(100);
+}
+
 function hexagon(x){
 forward(x);
 right(60);
@@ -33,8 +37,24 @@ forward(x);
 right(60);
 }
 
+function shape(type, size){
+    if (type==='triangle'){
+        triangle(size);
+    }
+    else if (type==='square'){
+        square(size);
+    }
+    else if (type==='hexagon'){
+        hexagon(size);
+    }
+}
 // Type your function call below
-house(100);
+square(100);
+triangle(70);
 hexagon(50);
+
+shape('square',100);
+shape('triangle',70);
+shape('hexagon',50);
 
 stamp();
